e6c563267c6fd0bffd4d309da4b8d33c39e26a50
lbayl0721 <baylessluke@gmail.com>
2017-07-06 18:35:11 -0400

    Traced issue with drive to a lack of slew rate

--- src/drive.c
+++ src/drive.c
@@ -4,16 +4,16 @@ Drive Code
 #include "main.h"
 
 void driveSet (int left, int right) {
-blrsMotorSet(MOTOR_LEFT_FRONT, left, false);
-blrsMotorSet(MOTOR_LEFT_BACK, left, false);
-blrsMotorSet(MOTOR_RIGHT_FRONT, right, false);
-blrsMotorSet(MOTOR_RIGHT_BACK, right, false);
+  blrsMotorSet(MOTOR_LEFT_FRONT, left, false);
+  blrsMotorSet(MOTOR_LEFT_BACK, left, false);
+  blrsMotorSet(MOTOR_RIGHT_FRONT, right, false);
+  blrsMotorSet(MOTOR_RIGHT_BACK, right, false);
 
 }
 
 void driveInit(){
-  blrsMotorInit(MOTOR_LEFT_FRONT,false,NULL,NULL);
-  blrsMotorInit(MOTOR_LEFT_BACK,false,NULL,NULL);
-  blrsMotorInit(MOTOR_RIGHT_FRONT,false,NULL,NULL);
-  blrsMotorInit(MOTOR_RIGHT_BACK,false,NULL,NULL);
+  blrsMotorInit(MOTOR_LEFT_FRONT,false, 0.5f,NULL);
+  blrsMotorInit(MOTOR_LEFT_BACK,false, 0.5f,NULL);
+  blrsMotorInit(MOTOR_RIGHT_FRONT,false, 0.5f,NULL);
+  blrsMotorInit(MOTOR_RIGHT_BACK,false, 0.5f,NULL);
 }
