353b3ba63f0424378b4a92819aa2dcaae1d2ed4b
Candy Cron <torcub1@gmail.com>
2017-11-23 11:53:35 -0500

    Shell Autons Uploaded

--- include/config.h
+++ include/config.h
@@ -1,7 +1,7 @@
 #ifndef CONFIG_H_
 #define CONFIG_H_
 
-#define CLAW 1
+#define CLAW 1     // Motors
 #define LEFT_FRONT 2
 #define LEFT_BACK 3
 #define RIGHT_FRONT 4
@@ -10,10 +10,10 @@
 #define RACK 7
 #define MOGO 8
 
-#define ENCODER_LEFT_TOP 2
+#define ENCODER_LEFT_TOP 2     // Encoders
 #define ENCODER_LEFT_BOTTOM 3
 
-#define LIFT_POT 1
+#define LIFT_POT 1     // Potentiometers
 #define AUTON_POT 4
 #define MOGO_POT 5
 
